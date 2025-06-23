f7da5cd316b41c8182fea320570f7e9690a2a4a6
@MahdiOR <mahdi.ostadrahim@gmail.com>
2025-06-23 15:07:21 +0330

    fix bar.bat for integration generation

--- ProtoFiles_Schema/bar.bat
+++ ProtoFiles_Schema/bar.bat
@@ -1,8 +1,8 @@
 @echo off
 for %%f in (*.proto) do (
     set "file=%%~nf"
-    if not exist "%%~nf" mkdir "%%~nf"
-    protoc --go_out=%%~nf --go_opt=paths=source_relative ^
-           --go-grpc_out=%%~nf --go-grpc_opt=paths=source_relative ^
+    if not exist "../integration/%%~nf" mkdir "../integration/%%~nf"
+    protoc --go_out=../integration/%%~nf --go_opt=paths=source_relative ^
+           --go-grpc_out=../integration/%%~nf --go-grpc_opt=paths=source_relative ^
            %%f
 )
